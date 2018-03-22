bdbd05d6d08b4ec95b1f71d76735a25a734c5ee1
Hieu Le <trunghieule94@gmail.com>
2018-03-22 13:49:07 -0500

    Allow passing a custom comparator for binary search algorithms

--- src/com/hieule/algorithm/BinarySearch.java
+++ src/com/hieule/algorithm/BinarySearch.java
@@ -1,5 +1,6 @@
 package com.hieule.algorithm;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -26,6 +27,20 @@ public final class BinarySearch {
         return low;
     }
 
+    public static <T> int lowerBound(List<? extends T> list, T value, Comparator<? super T> comp) {
+        int low = 0;
+        int high = list.size();
+        while (low < high) {
+            final int mid = low + ((high - low) >> 1);
+            if (comp.compare(list.get(mid), value) >= 0)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+
     /**
      * Returns the index of the first element from list which compares greater than value.
      */
@@ -43,11 +58,31 @@ public final class BinarySearch {
         return low;
     }
 
+    public static <T> int upperBound(List<? extends T> list, T value, Comparator<? super T> comp) {
+        int low = 0;
+        int high = list.size();
+        while (low < high) {
+            final int mid = low + ((high - low) >> 1);
+            if (comp.compare(list.get(mid), value) > 0)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+
     /**
      * Returns true if any element from list is equivalent to value, and false otherwise.
      */
     public static <T> boolean binarySearch(List<? extends Comparable<? super T>> list, T value) {
         int index = lowerBound(list, value);
         return index != list.size() && list.get(index).compareTo(value) == 0;
+    }
+
+    public static <T> boolean binarySearch(List<? extends T> list, T value,
+                                           Comparator<? super T> comp) {
+        int index = lowerBound(list, value, comp);
+        return index != list.size() && comp.compare(list.get(index), value) == 0;
     }
 }
